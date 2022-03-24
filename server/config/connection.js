2f5b58c6403360065f845f71af158b5ad8d16b96
plaird730 <plaird730@gmail.com>
2022-03-24 15:21:21 -0400

    Added ssl to production db connection, added config for local development

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,23 @@
 const Sequelize = require("sequelize");
+const dbconfig = require("./dbconfig");
 require("dotenv").config();
+const { DATABASE_URL } = process.env;
+const { LOCALDATABASE_URL } = dbconfig;
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL || "postgres://postgres:root@localhost:5432/hsmvp"
-);
+if (!DATABASE_URL || !LOCALDATABASE_URL) {
+  throw new Error(
+    "Error reading database connection string in the environment variables."
+  );
+}
+
+const options = DATABASE_URL.includes("localhost")
+  ? dbconfig.OPTIONS["localhost"]
+  : dbconfig.OPTIONS["production"];
+
+if (!options) {
+  throw new Error("Error parsing database connection string.");
+}
+
+const sequelize = new Sequelize(DATABASE_URL || LOCALDATABASE_URL);
 
 module.exports = sequelize;
